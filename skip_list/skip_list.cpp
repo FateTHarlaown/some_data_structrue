5088e8af024dbbed7b4c75e64df04aa1116615f9
FateTHarlaown <623239185@qq.com>
2017-07-10 22:59:13 +0800

    2017/7/10 22:58

--- skip_list/skip_list.cpp
+++ skip_list/skip_list.cpp
@@ -16,7 +16,38 @@ SkipList::SkipList(int maxLevel, float skipListP):MAX_LEVEL(maxLevel),
 }
 
 template <class KeyType, class DataType>
-bool SkipList<KeyType, DataType>::searchNode(const KeyType & key, const DataType & value)
+bool SkipList<KeyType, DataType>::searchNode(const KeyType & key, DataType & value)
 {
+    ListNode * x = &head;
+    for(int i = level_num; i >= 0; i--)
+    {
+        while (x->forwards[i] != NULL && x->forwards[i]->key < key)//这意味着KeyType类型必须有小于运算符“<”，若没有
+        {                                                          //或者其表现与所需不符合，需要自己重载该运算符
+            x = x->forwards[i];
+        }
+    }
+    x = x->forwards[0];
+    if(x != NULL && x->key == key)
+    {
+        value = x->value;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <class KeyType, class DataType>
+bool SkipList<KeyType, DataType>::insertNode(const KeyType &key, const DataType &value)
+{
+    ListNode * x = &head;
+    for(int i = level_num; i >= 0; i--)
+    {
+        while (x->forwards[i] != NULL && x->forwards[i]->key < key)//这意味着KeyType类型必须有小于运算符“<”，若没有
+        {                                                          //或者其表现与所需不符合，需要自己重载该运算符
+            x = x->forwards[i];
+        }
+    }
 
 }
