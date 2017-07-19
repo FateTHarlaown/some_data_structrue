e4707920387d4fa8145ef8c8d61e3562c2521735
zhang <623239185@qq.com>
2017-07-19 20:07:06 +0800

    it can run for the first time....but it is so foolish!

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-    srand((unsigned int)time(NULL));
     SkipList<int, int> SList;
     SList.insertNode(1,2);
     SList.insertNode(2,2);
@@ -14,12 +13,33 @@ int main()
         SList.displayByLevel(i);
     }
     int ans;
-    if (SList.searchNode(100, ans))
+    if (SList.searchNode(5, ans))
     {
         cout << "Find val for key 5:  " << ans << endl;
     }
     else
         cout << "fuck!!" << endl;
+    if (SList.deleteNode(3))
+    {
+        cout << "delete key 3" << endl;
+        for(int i = SList.getLevel(); i >= 0; i--)
+        {
+            SList.displayByLevel(i);
+        }
+    }
+    else
+        cout << "Oh no!" << endl;
+    if (SList.updateNode(100, -20))
+    {
+        cout << "delete key 3" << endl;
+        for(int i = SList.getLevel(); i >= 0; i--)
+        {
+            SList.displayByLevel(i);
+        }
+    }
+    else
+        cout << "Oh no!" << endl;
+
     return 0;
 
 }
